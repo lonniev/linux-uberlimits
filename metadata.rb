8ef4321e7a9b3339e8ce6d5e5c367e487ba6c6a4
Lonnie VanZandt <lonniev@gmail.com>
2017-03-06 19:58:13 -0700

    Indicate Ubuntu specificity

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures Uber Limits for Ubuntu Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+supports         'ubuntu'
+
 depends 'limits'
